9c67fb53d99083af933b6daf752a697f66a96999
SonNguyen <quangson3591@gmail.com>
2024-03-29 07:11:29 +0700

    cm

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Son Nguyen\OneDrive - Rackspace Inc\RAX\Github\rax_dbt_capstone\venv
+set VIRTUAL_ENV=C:\Users\son3435\Python\rax_dbt_capstone\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
